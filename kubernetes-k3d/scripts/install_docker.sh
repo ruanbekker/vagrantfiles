b54dfe8ce6a2527fa2e4673ec0e4f74ae5013835
Ruan Bekker <ruan.ru.bekker@gmail.com>
2019-12-11 15:38:23 +0200

    Create install_docker.sh

---
+++ kubernetes-k3d/scripts/install_docker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo apt update
+sudo apt remove docker docker-engine -y
+sudo apt install linux-image-extra-virtual -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+sudo apt install docker-ce -y
+
+sudo systemctl enable docker
+sudo systemctl restart docker
+sudo usermod -aG docker vagrant
+sudo docker run hello-world
